74ee7589c4f3e98dfc0846219fc091052d640b1e
Hoomaan <hormozdi.hooman@gmail.com>
2022-09-15 14:13:37 +0430

    support scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { StrictMode } from "react";
 
+import "./styles.scss";
+
 const App = () => (
   <StrictMode>
     <h1>Hi World !!!</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
