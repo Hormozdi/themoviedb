3807b30bf8535f75da3469a47339f858f6142abd
Hoomaan <hormozdi.hooman@gmail.com>
2022-09-15 23:45:24 +0430

    add react-toastify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { StrictMode } from "react";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import router from "./router";
 
+import "react-toastify/dist/ReactToastify.css";
 import "./styles.scss";
 
 const App = () => (
   <StrictMode>
+    <ToastContainer />
     <RouterProvider router={router} />
   </StrictMode>
 );
