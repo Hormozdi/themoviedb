f8a0d2a5c43f87d221e61b52816b6dba21ded164
Hoomaan <hormozdi.hooman@gmail.com>
2022-09-15 20:23:52 +0430

    create header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./style.scss";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  const menuItems = [{ to: "/", title: "Home" }];
+
+  return (
+    <div className="header-wrapper">
+      <Link to="/">
+        <img src="assets/logo.svg" className="logo" />
+      </Link>
+      <div className="menu">
+        {menuItems?.map((item) => (
+          <Link key={item.to} to={item.to}>
+            {item.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
